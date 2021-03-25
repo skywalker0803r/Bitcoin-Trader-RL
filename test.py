dcc4a02d4d08e6e8e7ff5ba51b0e73145393c390
skywalker0803r <41248543+skywalker0803r@users.noreply.github.com>
2021-03-25 16:54:36 +0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,30 @@
+import gym
+import pandas as pd
+
+from stable_baselines.common.policies import MlpPolicy
+from stable_baselines.common.vec_env import DummyVecEnv
+from stable_baselines import A2C
+
+from env.BitcoinTradingEnv import BitcoinTradingEnv
+
+df = pd.read_csv('./data/bitstamp.csv')
+df = df.sort_values('Timestamp')
+
+slice_point = int(len(df) - 50000)
+
+test_df = df[slice_point:]
+
+total_timesteps = 2000
+
+model = A2C.load("./model/model{}".format(total_timesteps))
+
+test_env = DummyVecEnv(
+    [lambda: BitcoinTradingEnv(test_df, serial=True)])
+
+obs = test_env.reset()
+for i in range(50000):
+    action, _states = model.predict(obs)
+    obs, rewards, done, info = test_env.step(action)
+    test_env.render(mode="human")
+
+test_env.close()
---
+++ train.py
@@ -0,0 +1,28 @@
+import gym
+import pandas as pd
+
+from stable_baselines.common.policies import MlpPolicy
+from stable_baselines.common.vec_env import DummyVecEnv
+from stable_baselines import A2C
+
+from env.BitcoinTradingEnv import BitcoinTradingEnv
+
+df = pd.read_csv('./data/bitstamp.csv')
+df = df.sort_values('Timestamp')
+
+slice_point = int(len(df) - 50000)
+
+train_df = df[:slice_point]
+test_df = df[slice_point:]
+
+train_env = DummyVecEnv(
+    [lambda: BitcoinTradingEnv(train_df, serial=True)])
+
+model = A2C(MlpPolicy, train_env, verbose=1,
+            tensorboard_log="./tensorboard/")
+total_timesteps = 20000
+model.learn(total_timesteps=total_timesteps)
+
+model.save("./model/model{}".format(total_timesteps))
+
+print('model training done!')
